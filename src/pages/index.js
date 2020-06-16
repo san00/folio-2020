cd2dcc7bacb2f74f2068e4c0ed456b45de1aec51
sanscomp <sanscomp@anslems-iMac.lan>
2020-06-16 13:01:44 +0100

    Set up home page & global styles for site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
-import React from "react"
+import React from 'react';
+import Layout from '../components/layout';
 
-export default function Home() {
-  return <div>Hi world!</div>
-}
+export default () => (
+  <Layout>
+    <h1>hello this is my site</h1>
+    <p>welcome folio</p>
+  </Layout>
+);
