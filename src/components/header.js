362b5610f19a0fb1fc910abefa97a8a0e7d65d60
sanscomp <sanscomp@anslems-iMac.lan>
2020-06-16 13:02:24 +0100

    Add site header

---
+++ src/components/header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { css } from '@emotion/core';
+import { Link } from 'gatsby';
+
+const NavLink = styled(Link)`
+  color: #222;
+  font-size: 1rem;
+  font-weight: normal;
+  line-height: 1;
+  margin: 0 0.5rem 0 0;
+  padding: 0.25rem;
+  text-decoration: none;
+`;
+
+const Header = () => (
+  <header>
+    <NavLink to="/">FOLIO 2020</NavLink>
+  </header>
+);
+
+export default Header;
